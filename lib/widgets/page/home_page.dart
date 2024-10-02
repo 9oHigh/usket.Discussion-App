25dc0f74af4de3237cf65caa9235995835f88678
9oHigh <wkdska957@naver.com>
2024-10-02 20:56:25 +0900

    feat: 홈화면 ui 구성, 홈화면에 사용될 roomListModel 구현

---
+++ lib/widgets/page/home_page.dart
@@ -0,0 +1,65 @@
+import 'package:app_team1/widgets/screen/favorite_screen.dart';
+import 'package:app_team1/widgets/screen/home_screen.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _widgetOptions = <Widget>[
+    HomeScreen(),
+    FavoriteScreen(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_selectedIndex == 0 ? '방 목록' : '마이'),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {
+              // MARK: - context.go('/filter')
+            },
+            icon: const Icon(Icons.filter_alt),
+          ),
+        ],
+      ),
+      body: _widgetOptions.elementAt(_selectedIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: '홈',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: '마이',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: _selectedIndex == 0 ? Colors.purple : Colors.red,
+        onTap: _onItemTapped,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // context.go('/create_room')
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
