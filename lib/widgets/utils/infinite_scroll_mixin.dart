291e59777101d2490678a8a9e373044e394bd982
9oHigh <wkdska957@naver.com>
2024-10-08 21:09:01 +0900

    refactor: mixin infinite scroll 리팩토링

--- lib/widgets/utils/infinite_scroll_mixin.dart
+++ lib/widgets/utils/infinite_scroll_mixin.dart
@@ -1,45 +1,24 @@
 import 'package:flutter/material.dart';
-import '../../model/room.dart';
 
-mixin InfiniteScrollMixin<T, W extends StatefulWidget> on State<W> {
-  List<T> roomList = [];
+mixin InfiniteScrollMixin<T extends StatefulWidget> on State<T> {
   bool isLoading = false;
-  String? cursorId; // 페이징을 위한 커서 ID
+  String? cursorId;
 
-  void initializeScrollController(
-      ScrollController controller, Future<List<T>> Function() fetchData) {
-    controller.addListener(() {
+  initializeScrollController(
+      ScrollController controller, Future<void> Function() fetchData) {
+    controller.addListener(() async {
       if (controller.position.pixels == controller.position.maxScrollExtent &&
           !isLoading) {
-        loadMoreData(fetchData);
+        setState(() {
+          isLoading = true;
+        });
+        await fetchData();
+        // 딜레이를 통해서 확인 하기
+        await Future.delayed(const Duration(seconds: 1));
+        setState(() {
+          isLoading = false;
+        });
       }
     });
-  }
-
-  Future<void> loadMoreData(Future<List<T>> Function() fetchData) async {
-  setState(() {
-    if (isLoading) return; // 이미 로딩 중이면 종료
-    isLoading = true; // 로딩 시작
-  });
-
-  final newRooms = await fetchData(); // 데이터 로드
-  setState(() {
-    roomList.addAll(newRooms); // 새로운 룸들 추가
-    if (newRooms.isNotEmpty) {
-      cursorId = getRoomId(newRooms.last); // 커서 ID 업데이트
-      print('새로운 방이 추가됨');
-    } else {
-      cursorId = null; // 더 이상 데이터 없음
-    }
-    isLoading = false; // 로딩 종료
-  });
-}
-
-  // ID를 추출하는 메서드 정의
-  String getRoomId(T room) {
-    if (room is Room) {
-      return room.roomId.toString(); // Room 클래스의 ID를 문자열로 반환
-    }
-    throw Exception('Unsupported type: ${room.runtimeType}'); // 타입이 지원되지 않을 경우 예외 발생
   }
 }
