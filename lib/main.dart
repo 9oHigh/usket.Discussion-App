c1528cf24fc719b7bd5ec33d0823a1e91a434e5f
9oHigh <wkdska957@naver.com>
2024-10-11 16:18:10 +0900

    feat: 자신이 만든 방인지 파악하기 위해 홈화면 진입시 플레이어 생성 + ID 관리

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,23 @@
+import 'package:app_team1/model/player.dart';
 import 'package:app_team1/router.dart';
+import 'package:app_team1/services/api_service.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:timezone/data/latest.dart' as tz;
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   tz.initializeTimeZones();
+  await _initializeUser();
   runApp(const AppTeam1());
+}
+
+_initializeUser() async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  Player? player = await ApiService().getOrCreatePlayer();
+  if (player != null) {
+    prefs.setInt("playerId", player.id);
+  }
 }
 
 class AppTeam1 extends StatelessWidget {
